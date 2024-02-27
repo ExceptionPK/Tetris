581dc8f028d9481857d0bf527fc62117eae458f5
ExceptionPK <saleunomas@gmail.com>
2024-02-27 18:18:05 +0100

    Ordenado

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Tetris from './Tetris';
+import Tetris from './Components/Tetris';
 
 const App = () => {
   return (
